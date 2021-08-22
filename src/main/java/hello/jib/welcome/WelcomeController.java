f889c62f274b1d4c8408595f9b581c3d88efad00
오석근/주문개발팀 <dean.oh@musinsa.com>
2021-08-22 12:02:00 +0900

    Add divide method for APM(newrelic) test.

--- src/main/java/hello/jib/welcome/WelcomeController.java
+++ src/main/java/hello/jib/welcome/WelcomeController.java
@@ -2,6 +2,7 @@ package hello.jib.welcome;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,5 +14,11 @@ public class WelcomeController {
     @ResponseBody
     public String welcome() {
         return "Welcome!";
+    }
+
+    @GetMapping("/divide/{p1}/{p2}")
+    @ResponseBody
+    public int divide(@PathVariable("p1") int p1, @PathVariable("p2") int p2) {
+        return p1 / p2;
     }
 }
