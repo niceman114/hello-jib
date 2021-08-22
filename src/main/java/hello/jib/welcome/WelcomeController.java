c96a8c4f65291e618d2b30fa9e844be0509953fe
오석근/주문개발팀 <dean.oh@musinsa.com>
2021-08-22 10:36:27 +0900

    Add WelcomeController.

---
+++ src/main/java/hello/jib/welcome/WelcomeController.java
@@ -0,0 +1,17 @@
+package hello.jib.welcome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class WelcomeController {
+
+    @GetMapping
+    @ResponseBody
+    public String welcome() {
+        return "Welcome!";
+    }
+}
