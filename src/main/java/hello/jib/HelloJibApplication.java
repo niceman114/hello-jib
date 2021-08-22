d425396410b4ffbb2c164c13be5143dd903ee302
오석근/주문개발팀 <dean.oh@musinsa.com>
2021-08-22 10:35:46 +0900

    Initialize.

---
+++ src/main/java/hello/jib/HelloJibApplication.java
@@ -0,0 +1,13 @@
+package hello.jib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJibApplication.class, args);
+    }
+
+}
